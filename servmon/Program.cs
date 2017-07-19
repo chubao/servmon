49bb9cc63781327ec43268e63c9c286310972b71
prasong <prasong@hotmail.com>
2017-07-19 15:40:58 +0700

    update quartz and Topshelf

--- servmon/Program.cs
+++ servmon/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace servmon
 {
@@ -10,6 +11,21 @@ namespace servmon
     {
         static void Main(string[] args)
         {
+            HostFactory.Run(host =>
+            {
+                host.Service<ServmonService>(s =>
+                {
+                    s.ConstructUsing(name => new ServmonService(name));
+                    s.WhenStarted((tc, hostControl) => tc.Start(hostControl));
+                    s.WhenStopped((tc, hostControl) => tc.Stop(hostControl));
+                });
+
+                host.RunAsLocalSystem();
+                host.StartAutomaticallyDelayed();
+                host.SetDescription(Configuration.ServiceDescription);
+                host.SetDisplayName(Configuration.ServiceDisplayName);
+                host.SetServiceName(Configuration.ServiceName);
+            });
         }
     }
 }
---
+++ servmon/ServmonService.cs
@@ -0,0 +1,83 @@
+﻿
+using log4net;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.Runtime;
+
+namespace servmon
+{
+    class ServmonService : ServiceControl
+    {
+        private readonly ILog logger;
+        private IScheduler scheduler;
+        private QuartzServer server;
+        public ServmonService(HostSettings hostsetting)
+        {
+            try
+            {
+                logger = LogManager.GetLogger(GetType());
+                Initialize();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+
+        #region Initial Server
+        public virtual void Initialize()
+        {
+            try
+            {
+                #region initial Quartz Server.
+                server = QuartzServerFactory.CreateServer();
+                server.Initialize();
+                scheduler = server.GetScheduler();
+                #endregion
+
+            }
+            catch (Exception e)
+            {
+                logger.Error("Server initialization failure:" + e.Message, e);
+                throw;
+            }
+        }
+        #endregion
+
+        public bool Start(HostControl hostControl)
+        {
+            try
+            {
+                scheduler.Start();
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(string.Format("Service start failure: {0}", ex.Message), ex);
+                throw (ex);
+            }
+            logger.Info("Service started successfully");
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            try
+            {
+                scheduler.Shutdown();
+                scheduler = null;
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(string.Format("Scheduler stop failure: {0}", ex.Message), ex);
+                throw (ex);
+            }
+            logger.Info("Service stop successfully");
+            return true;
+        }
+    }
+}
